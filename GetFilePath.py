8dacecd0a6c556d68d70db808e591fb470e3709c
F2btw <romafedorov567@gmail.com>
2024-10-27 16:19:36 +0300

    GetFilePath.py

--- GetFilePath.py
+++ GetFilePath.py
@@ -1,31 +1,18 @@
-import tkinter as tk
-from tkinter import filedialog
+import base64
+from flet import *
+image = ft.Image(visible=False,fit= ImageFit.FILL)
+    def get_file_path(e: FilePickerResultEvent):
+     global file_path
 
-def get_file_path():
-    # Create a hidden root window
-    root = tk.Tk()
-    root.withdraw()  # Hide the root window
+     print(e.files)
+     if e.files and len(e.files):
+            file_path = e.files[0].path
+            
+            with open(e.files[0].path, 'rb')as r:
+             image.src_base64 = base64.b64encode(r.read()).decode('utf-8')
+             image.visible = True
+             page.update()
 
-    # Open the file dialog and get the file path
-    file_path = filedialog.askopenfilename(
-        title="Select a File",
-        filetypes=[("All Files", "*.*")]  # You can specify file types here
-    )
-
-    # Check if a file was selected
-    if file_path:
-        print(f"Selected file: {file_path}")
-    else:
-        print("No file selected.")
-
-    return file_path
-
-# Example usage
-file_path = get_file_path()
-
-if __name__ == "__main__":
-    print(file_path)
-
-
-
-
+#кнопка 
+ElevatedButton('ВЫБЕРИТЕ ИЗОБРАЖЕНИЕ',width=300,height=30,on_click=lambda _:File_Picker.pick_files(allow_multiple=False,
+            allowed_extensions=['jpg','jpeg','png']))
